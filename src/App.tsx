663f2e74b426c304e48f057170b6674a7a4b79f0
Karen Dantas <karennobre74@gmail.com>
2024-06-08 17:33:31 -0300

    feat: start form validation with zod and react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import { Input } from "./components/input"
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+import { Input } from "./components/input";
+
+const NewCustomerFormSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  cpf: z.string(),
+  password: z.string(),
+
+})
+
+type NewCustomerFromType = z.infer<typeof NewCustomerFormSchema>
 
 function App() {
- 
+  const {register, handleSubmit} = useForm<NewCustomerFromType>({
+    resolver: zodResolver(NewCustomerFormSchema)
+  });
+
+  function handleCreateCustomer (data: NewCustomerFromType ){
+    console.log(data);
+  }
+
   return (
    <main className="max-w-6xl h-screen mx-auto flex justify-center items-center ">
       <div className="w-[600px] h-[600px] flex justify-center items-center flex-col border border-pink-500 rounded-md mx-5 shadow-xl shadow-pink-600">
    
         <h2 className="text-2xl mb-11">Cadastro</h2>
 
-        <form action="" className="w-[80%] flex flex-col items-center gap-6 ">
+        <form onSubmit={handleSubmit(handleCreateCustomer)} className="w-[80%] flex flex-col items-center gap-6 ">
 
-          <Input type="text" placeholder=" Nome"/>
+          <Input type="text" placeholder=" Nome" {...register("name")}/>
 
-          <Input type="email" placeholder="Email"/>
+          <Input type="email" placeholder="Email" {...register("email")}/>
 
-          <Input type="text" placeholder="CPF"/>
+          <Input type="text" placeholder="CPF" {...register("cpf")}/>
 
-          <Input type="password" placeholder="Senha"/>
+          <Input type="password" placeholder="Senha" {...register("password")}/>
 
-          <button className="bg-pink-500 hover:bg-pink-400 w-[50%] py-4 px-3 mt-5 rounded-md md:text-xl"> Confirmar</button>
+          <button type="submit" className="bg-pink-500 hover:bg-pink-400 w-[50%] py-4 px-3 mt-5 rounded-md md:text-xl"> Confirmar</button>
         </form>
   
       </div>
