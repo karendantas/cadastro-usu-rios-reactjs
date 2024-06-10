5796138e3c51f5863b61dbefba41c3a044007703
Karen Dantas <karennobre74@gmail.com>
2024-06-10 11:15:25 -0300

    feat: create function to add and delete users using users api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,31 +3,31 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Input } from "../components/input";
-import { useContext } from "react";
-import { UserContext } from "../contexts/usersContexts";
 import { useNavigate } from "react-router-dom";
+import { api } from "../services/api";
 
 const NewCustomerFormSchema = z.object({
   name: z.string(),
   email: z.string(),
-  cpf: z.string(),
   age: z.number(),
-  password: z.string(),
 
 })
 
 type NewCustomerFromType = z.infer<typeof NewCustomerFormSchema>
 
 function Home() {
-  const {users} = useContext(UserContext);
   const navigate = useNavigate();
 
   const {register, handleSubmit} = useForm<NewCustomerFromType>({
     resolver: zodResolver(NewCustomerFormSchema)
   });
 
-  function handleCreateCustomer (data: NewCustomerFromType ){
-    console.log(data);
+  async function handleCreateCustomer (data: NewCustomerFromType ){
+    await api.post('/users', {
+      name: data.name,
+      email: data.email,
+      age: data.age
+    })
   }
 
   function handleGoUserListPage(){
@@ -36,23 +36,20 @@ function Home() {
 
   return (
    <main className="max-w-6xl h-screen mx-auto flex flex-col justify-center items-center ">
-      <div className="w-[600px] h-[600px] flex justify-center items-center flex-col border border-pink-500 rounded-md mx-5 shadow-xl shadow-pink-600">
+      <div className="w-[600px] h-[500px] flex justify-center items-center flex-col border border-pink-500 rounded-md mx-5 shadow-xl shadow-pink-600">
    
         <h2 className="text-2xl mb-11">Cadastro</h2>
 
-        <form onSubmit={handleSubmit(handleCreateCustomer)} className="w-[80%] flex flex-col items-center gap-6 ">
+        <form onSubmit={handleSubmit(handleCreateCustomer)} className="w-[80%] flex flex-col items-center gap-6">
 
-          <Input type="text" placeholder=" Nome" {...register("name")}/>
+          <input type="text" placeholder=" Nome" {...register("name")}/>
 
-          <Input type="email" placeholder="Email" {...register("email")}/>
+          <input type="email" placeholder="Email" {...register("email")}/>
 
-          <Input type="text" placeholder="CPF" {...register("cpf")}/>
+          <input type = "text" placeholder="Idade" {...register("age")} />
 
-          <Input type = "number" placeholder="Idade" {...register("age")} />
 
-          <Input type="password" placeholder="Senha" {...register("password")}/>
-
-          <button type="submit" className="bg-pink-500 hover:bg-pink-400 w-[50%] py-4 px-3 mt-5 rounded-md md:text-xl"> Confirmar</button>
+          <button className="bg-pink-500 hover:bg-pink-400 w-[50%] py-4 px-3 mt-5 rounded-md md:text-xl"> Confirmar</button>
         </form>
 
       </div>
--- src/pages/UsersList.tsx
+++ src/pages/UsersList.tsx
@@ -1,21 +1,22 @@
 import { useContext } from "react"
 import { UserContext } from "../contexts/usersContexts"
 import { useNavigate } from "react-router-dom";
+import { api } from "../services/api";
 
 export function UsersList (){
     const {users} = useContext(UserContext);
     const navigate = useNavigate();
 
-    function handleDeleteUser(userId: string){
-        console.log(userId)
+    async function handleDeleteUser(userId: string){
+        await api.delete(`/users/${userId}`)
     }
     return (
         <main className="max-w-6xl h-screen mx-auto flex  flex-col justify-center items-center ">
             <div className = "w-full flex flex-col gap-7 ">
                 {users.map((user) => {
                     return (
-                        <div className="bg-pink-500 w-full py-5 px-3 rounded-md flex justify-between items-center">
-                            <div className="flex flex-col ">
+                        <div key={user.id}className="bg-pink-500 w-full py-5 px-3 rounded-md flex justify-between items-center">
+                            <div key={user.id} className="flex flex-col ">
                                 <strong className="text-2xl text-slate-800">{user.name}</strong>
                                 <span className="text-lg text-slate-200">email: {user.email}</span>
                                 <span className="text-lg text-slate-200">idade: {user.age} anos</span>
@@ -24,6 +25,7 @@ export function UsersList (){
                             <button 
                                 className="h-[40px] w-[40px] bg-purple-700 flex items-center justify-center rounded-md"
                                 onClick={ () => handleDeleteUser(user.id)}
+                                key={user.id}
                                 >
                                 <strong> X </strong>
                             </button>
